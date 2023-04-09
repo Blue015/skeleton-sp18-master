632ab0144fb567e94671f89ae7518b28b30d9919
Blue015 <113355359+Blue015@users.noreply.github.com>
2023-04-09 16:00:08 +0800

    The seventh timet

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -23,8 +23,8 @@ public class ArrayDeque<T> {
         if (size == arr.length) {
             brr = (T[]) new Object[size * 2];
             System.arraycopy(arr, 0, brr, 0, size);
-            brr[size] = item;
             arr = brr;
+            arr[size] = item;
         } else if (size == 0) {
 //            brr = (T[]) new Object[1];
 //            brr[size] = item;
@@ -84,17 +84,17 @@ public class ArrayDeque<T> {
         }
     }
 
-//    public static void main(String[] args){
-//        ArrayDeque<Integer> l = new ArrayDeque<>();
-//        l.addLast(2);
-//        l.addLast(3);
-//        l.addLast(2);
-//        l.addLast(2);
-//        l.addLast(2);
-//        l.addLast(2);
-//        l.addLast(2);
-//        l.addLast(2);
-//        l.addLast(6);
-//        l.addLast(4);
-//    }
+    public static void main(String[] args) {
+        ArrayDeque<Integer> l = new ArrayDeque<>();
+        l.addLast(2);
+        l.addLast(3);
+        l.addLast(2);
+        l.addLast(2);
+        l.addLast(2);
+        l.addLast(2);
+        l.addLast(2);
+        l.addLast(2);
+        l.addLast(6);
+        l.addLast(4);
+    }
 }
