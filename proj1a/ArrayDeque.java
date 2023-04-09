e8aa0ce83ad4180c588bc4fe3a3140da56768c90
Blue015 <113355359+Blue015@users.noreply.github.com>
2023-04-09 15:40:28 +0800

    The sixth time

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -9,9 +9,9 @@ public class ArrayDeque<T> {
     }
 
     public void addFirst(T item) {
-        T[] crr = (T[]) new Object[size+1];
-        if(size != 0) {
-            System.arraycopy(arr,0,crr,1,size);
+        T[] crr = (T[]) new Object[size + 1];
+        if (size != 0) {
+            System.arraycopy(arr, 0, crr, 1, size);
         }
         crr[0] = item;
         arr = crr;
@@ -20,15 +20,18 @@ public class ArrayDeque<T> {
 
     public void addLast(T item) {
         T[] brr;
-        if(size == arr.length) {
-            brr = (T[]) new Object[size*2];
-            System.arraycopy(arr,0,brr,0,size);
+        if (size == arr.length) {
+            brr = (T[]) new Object[size * 2];
+            System.arraycopy(arr, 0, brr, 0, size);
             brr[size] = item;
             arr = brr;
-        } else if(size == 0) {
-            brr = (T[]) new Object[1];
-            brr[size] = item;
-            arr = brr;
+        } else if (size == 0) {
+//            brr = (T[]) new Object[1];
+//            brr[size] = item;
+//            arr = brr;
+            arr[size] = item;
+        } else {
+            arr[size] = item;
         }
         size += 1;
     }
@@ -42,8 +45,8 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-        for(int i = 0;i < size;i++) {
-            if(i == size-1) {
+        for (int i = 0; i < size; i++) {
+            if (i == size - 1) {
                 System.out.print(arr[i]);
             } else {
                 System.out.print(arr[i] + " ");
@@ -52,37 +55,46 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
-        if(isEmpty()) {
+        if (isEmpty()) {
             return null;
         }
         T fpos = arr[0];
-        T[] drr = (T[]) new Object[size-1] ;
-        System.arraycopy(arr,1,drr,0,size-1);
+        T[] drr = (T[]) new Object[size - 1];
+        System.arraycopy(arr, 1, drr, 0, size - 1);
         arr = drr;
         size -= 1;
         return fpos;
     }
 
     public T removeLast() {
-        if(isEmpty()) {
+        if (isEmpty()) {
             return null;
         }
-        T lpos = arr[size-1];
-        arr[size-1] = null;
+        T lpos = arr[size - 1];
+        arr[size - 1] = null;
         size -= 1;
         return lpos;
     }
 
     public T get(int index) {
-        if(index+1 > size) {
+        if (index + 1 > size) {
             return null;
         } else {
             return arr[index];
         }
     }
-//
+
 //    public static void main(String[] args){
 //        ArrayDeque<Integer> l = new ArrayDeque<>();
 //        l.addLast(2);
+//        l.addLast(3);
+//        l.addLast(2);
+//        l.addLast(2);
+//        l.addLast(2);
+//        l.addLast(2);
+//        l.addLast(2);
+//        l.addLast(2);
+//        l.addLast(6);
+//        l.addLast(4);
 //    }
 }
