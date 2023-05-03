97473324ef6d4900ce819e72c85505f1938c4ab3
Blue015 <113355359+Blue015@users.noreply.github.com>
2023-05-03 20:53:13 +0800

    the third time

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,6 +3,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDequeGold {
+    private static Integer expected;
+    private static Integer actual;
+
     @Test
     public void testAd() {
         StudentArrayDeque<Integer> tests = new StudentArrayDeque<>();
@@ -19,28 +22,17 @@ public class TestArrayDequeGold {
             }
         }
 
-        Integer result4 = tests.removeFirst();
-        Integer result5 = testa.removeFirst();
-        assertEquals("\nremoveFirst(" + result4 + ")\n"
-                        + "removeFirst(" + result5 + ")\n" + "removeFirst()",
-                result4, result5);
+        for (int i = 0; i < 10; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
 
-        Integer result6 = tests.removeLast();
-        Integer result7 = testa.removeLast();
-        assertEquals("\nremoveLast(" + result6 + ")\n"
-                        + "removeLast(" + result7 + ")\n" + "removeLast()",
-                result7, result6);
-
-        Integer result0 = tests.removeFirst();
-        Integer result1 = testa.removeFirst();
-        assertEquals("\nremoveFirst(" + result0 + ")\n"
-                        + "removeFirst(" + result1 + ")\n" + "removeFirst()",
-                result1, result0);
-
-        Integer result2 = tests.removeLast();
-        Integer result3 = testa.removeLast();
-        assertEquals("\nremoveLast(" + result2 + ")\n"
-                        + "removeLast(" + result3 + ")\n" + "removeLast()",
-                result3, result2);
+            if (numberBetweenZeroAndOne < 0.5) {
+                expected = tests.removeLast();
+                actual = testa.removeLast();
+            } else {
+                expected = tests.removeFirst();
+                actual = testa.removeFirst();
+            }
+            assertEquals(expected + " not equal to " + actual, expected, actual);
+        }
     }
 }
